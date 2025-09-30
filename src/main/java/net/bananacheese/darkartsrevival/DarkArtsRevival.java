51e2aa47370cf930074d31c95b151649263a7f95
BananaCheese <bananacheese44@gmail.com>
2025-09-30 15:49:37 -0700

    commit

--- src/main/java/net/bananacheese/darkartsrevival/DarkArtsRevival.java
+++ src/main/java/net/bananacheese/darkartsrevival/DarkArtsRevival.java
@@ -1,5 +1,8 @@
 package net.bananacheese.darkartsrevival;
 
+import net.bananacheese.darkartsrevival.blocks.DABlocks;
+import net.bananacheese.darkartsrevival.blocks.entity.DABlockEntities;
+import net.bananacheese.darkartsrevival.commands.ReviveCommand;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,5 +14,8 @@ public class DarkArtsRevival implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        DABlocks.registerModBlocks();
+        DABlockEntities.registerBlockEntities();
+        ReviveCommand.register();
     }
 }

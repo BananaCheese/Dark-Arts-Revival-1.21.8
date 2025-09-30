d5b7b99d6a5e81ffc1ad13e7efb62fe5b79b56ea
BananaCheese <bananacheese44@gmail.com>
2025-09-30 15:35:08 -0700

    Initial commit

---
+++ src/main/java/net/bananacheese/darkartsrevival/DarkArtsRevivalDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bananacheese.darkartsrevival;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DarkArtsRevivalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
